09ea97634dd06e4ae69bd4ef926c78e3f5036339
ykob <info@tplh.net>
2023-04-15 12:24:09 +0900

    added camera and scene.

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,13 @@
-document.querySelector<HTMLDivElement>("#app")!.innerHTML = `the main page is running!`;
+import "./style.css";
+import * as THREE from "three";
 
-export {} 
+const scene = new THREE.Scene();
+const camera = new THREE.OrthographicCamera(-1, 1, 1, -1, 0, 1);
+const renderer = new THREE.WebGLRenderer();
+renderer.setSize(800, 600);
+
+document
+  .querySelector<HTMLDivElement>("#app")!
+  .appendChild(renderer.domElement);
+
+export {};
