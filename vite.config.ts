142d09283061c2bce4ccdf6ff691b3e0e44d1105
ykob <info@tplh.net>
2023-05-07 10:46:56 +0900

    update vite config to add server property.

--- vite.config.ts
+++ vite.config.ts
@@ -2,15 +2,18 @@ import { resolve } from "path";
 import { defineConfig } from "vite";
 
 export default defineConfig({
-  root: 'src',
-  publicDir: '../public/',
   build: {
-    outDir: '../dist',
+    outDir: "../dist",
     rollupOptions: {
       input: {
-        main: resolve(__dirname, 'src/index.html'),
-        nested: resolve(__dirname, 'src/nested/index.html'),
+        main: resolve(__dirname, "src/index.html"),
+        nested: resolve(__dirname, "src/nested/index.html"),
       },
     },
+  },
+  publicDir: "../public/",
+  root: "src",
+  server: {
+    host: true,
   },
 });
