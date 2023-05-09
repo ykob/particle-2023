f7b8d61242be198c93ddc060a6bbe2164ac00845
ykob <info@tplh.net>
2023-05-09 20:16:20 +0900

    added resize event.

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,25 @@
 import "./style.css";
 import * as THREE from "three";
+import { debounce, throttle } from "./utils";
 
 const scene = new THREE.Scene();
 const camera = new THREE.OrthographicCamera(-1, 1, 1, -1, 0, 1);
 const renderer = new THREE.WebGLRenderer();
-renderer.setSize(800, 600);
 
+const resize = () => {
+  renderer.setSize(0, 0, false);
+
+  const width = window.innerWidth;
+  const height = document.documentElement.clientHeight;
+
+  renderer.setSize(width, height, false);
+};
+
+window.addEventListener("resize", debounce(resize, 100));
+
+resize();
+
+renderer.domElement.classList.add("canvas-webgl");
 document
   .querySelector<HTMLDivElement>("#app")!
   .appendChild(renderer.domElement);
